cf1a8c78055d829708be905fb833980c063bab9e
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-25 19:00:36 +0300

    complete Arrays_Method

---
+++ Examples/Example_009_MethodArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 10, 6, 8, 5, 3, 4, 10, 7, 6 };
+
+int n = array.Length;
+int find = 6;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
